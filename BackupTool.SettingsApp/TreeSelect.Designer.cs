6f12f238002794ebed2b6e8b2f073e1d3a8565c5
lucaslittermentz <lucaslittermentz@users.noreply.github.com>
2017-05-29 08:38:42 -0300

    Include TreeSelect form

---
+++ BackupTool.SettingsApp/TreeSelect.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace BackupTool.SettingsApp {
+    partial class TreeSelect {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.treeViewMultiSelect = new System.Windows.Forms.TreeView();
+            this.buttonTreeViewConfirm = new System.Windows.Forms.Button();
+            this.buttonTreeViewCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeViewMultiSelect
+            // 
+            this.treeViewMultiSelect.Location = new System.Drawing.Point(5, 5);
+            this.treeViewMultiSelect.Name = "treeViewMultiSelect";
+            this.treeViewMultiSelect.Size = new System.Drawing.Size(389, 384);
+            this.treeViewMultiSelect.TabIndex = 0;
+            this.treeViewMultiSelect.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeViewMultiSelect_BeforeSelect);
+            this.treeViewMultiSelect.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeViewMultiSelect_MouseDown);
+            // 
+            // buttonTreeViewConfirm
+            // 
+            this.buttonTreeViewConfirm.Location = new System.Drawing.Point(320, 394);
+            this.buttonTreeViewConfirm.Name = "buttonTreeViewConfirm";
+            this.buttonTreeViewConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonTreeViewConfirm.TabIndex = 1;
+            this.buttonTreeViewConfirm.Text = "Confirmar";
+            this.buttonTreeViewConfirm.UseVisualStyleBackColor = true;
+            // 
+            // buttonTreeViewCancel
+            // 
+            this.buttonTreeViewCancel.Location = new System.Drawing.Point(239, 394);
+            this.buttonTreeViewCancel.Name = "buttonTreeViewCancel";
+            this.buttonTreeViewCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonTreeViewCancel.TabIndex = 2;
+            this.buttonTreeViewCancel.Text = "Cancelar";
+            this.buttonTreeViewCancel.UseVisualStyleBackColor = true;
+            // 
+            // TreeSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 422);
+            this.Controls.Add(this.buttonTreeViewCancel);
+            this.Controls.Add(this.buttonTreeViewConfirm);
+            this.Controls.Add(this.treeViewMultiSelect);
+            this.Name = "TreeSelect";
+            this.Text = "TreeSelect";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewMultiSelect;
+        private System.Windows.Forms.Button buttonTreeViewConfirm;
+        private System.Windows.Forms.Button buttonTreeViewCancel;
+    }
+}
---
+++ BackupTool.SettingsApp/TreeSelect.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackupTool.SettingsApp {
+    public partial class TreeSelect: Form {
+        public TreeSelect() {
+            selectedNodes = new List<TreeNode>();
+            boldFont = new Font("Arial", 10, FontStyle.Bold);
+            normalFont = new Font("Arial", 10, FontStyle.Regular);
+            InitializeComponent();
+        }
+
+        Font boldFont;
+        Font normalFont;
+        List<TreeNode> selectedNodes;
+        TreeNode previousNode;
+
+        private void TreeSelect_Shown(object sender, EventArgs e) {
+            String directory = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments);
+            BuildTree(directory, null);
+        }
+
+        private void BuildTree(String directory, TreeNode node) {
+            try {
+                String[] subdirectories = Directory.GetDirectories(directory);
+                foreach (String subdirectory in subdirectories) {
+                    String name = Path.GetFileName(subdirectory);
+                    TreeNode subnode = (node == null) ?
+                        treeViewMultiSelect.Nodes.Add(name) : node.Nodes.Add(name);
+                    subnode.NodeFont = boldFont;
+                    subnode.Text = subnode.Text; // google: 94354 treenode
+                    BuildTree(subdirectory, subnode);
+                }
+                String[] files = Directory.GetFiles(directory);
+                foreach (String file in files) {
+                    String name = Path.GetFileName(file);
+                    TreeNode subnode = (node == null) ?
+                        treeViewMultiSelect.Nodes.Add(name) : node.Nodes.Add(name);
+                    subnode.NodeFont = normalFont;
+                    subnode.Text = subnode.Text; // google: 94354 treenode
+                }
+            }
+            catch (Exception) {
+                // Ignore exception, e.g. when access to a folder denied
+            }
+        }
+
+        private void treeViewMultiSelect_BeforeSelect(object sender, TreeViewCancelEventArgs e) {
+            // cancel selection, the selection will be handled in MouseDown
+            e.Cancel = true;
+        }
+
+        private void treeViewMultiSelect_MouseDown(object sender, MouseEventArgs e) {
+            TreeNode currentNode = treeViewMultiSelect.GetNodeAt(e.Location);
+            if (currentNode == null) return;
+            currentNode.BackColor = treeViewMultiSelect.BackColor;
+            currentNode.ForeColor = treeViewMultiSelect.ForeColor;
+
+            bool control = (ModifierKeys == Keys.Control);
+            bool shift = (ModifierKeys == Keys.Shift);
+
+            if (control) {
+
+                // the node clicked with control button pressed:
+                // invert selection of the current node
+                List<TreeNode> addedNodes = new List<TreeNode>();
+                List<TreeNode> removedNodes = new List<TreeNode>();
+                if (!selectedNodes.Contains(currentNode)) {
+                    addedNodes.Add(currentNode);
+                    previousNode = currentNode;
+                }
+                else {
+                    removedNodes.Add(currentNode);
+                }
+                changeSelection(addedNodes, removedNodes);
+            }
+            else if (shift && previousNode != null) {
+                if (currentNode.Parent == previousNode.Parent) {
+                    // the node clicked with shift button pressed:
+                    // if current node and previously selected node
+                    // belongs to the same parent,
+                    // select range of nodes between these two
+                    List<TreeNode> addedNodes = new List<TreeNode>();
+                    List<TreeNode> removedNodes = new List<TreeNode>();
+                    bool selection = false;
+                    bool selectionEnd = false;
+
+                    TreeNodeCollection nodes = null;
+                    if (previousNode.Parent == null) {
+                        nodes = treeViewMultiSelect.Nodes;
+                    }
+                    else {
+                        nodes = previousNode.Parent.Nodes;
+                    }
+                    foreach (TreeNode n in nodes) {
+                        if (n == currentNode || n == previousNode) {
+                            if (selection) {
+                                selectionEnd = true;
+                            }
+                            if (!selection) {
+                                selection = true;
+                            }
+                        }
+                        if (selection && !selectedNodes.Contains(n)) {
+                            addedNodes.Add(n);
+                        }
+                        if (selectionEnd) {
+                            break;
+                        }
+                    }
+
+                    if (addedNodes.Count > 0) {
+                        changeSelection(addedNodes, removedNodes);
+                    }
+                }
+            }
+            else {
+                if (!currentNode.NodeFont.Bold) {
+                    // single click:
+                    // remove all selected nodes
+                    // and add current node
+                    List<TreeNode> addedNodes = new List<TreeNode>();
+                    List<TreeNode> removedNodes = new List<TreeNode>();
+                    removedNodes.AddRange(selectedNodes);
+                    if (removedNodes.Contains(currentNode)) {
+                        removedNodes.Remove(currentNode);
+                    }
+                    else {
+                        addedNodes.Add(currentNode);
+                    }
+                    changeSelection(addedNodes, removedNodes);
+                    previousNode = currentNode;
+                }
+            }
+        }
+
+        protected void changeSelection(List<TreeNode> addedNodes, List<TreeNode> removedNodes) {
+            foreach (TreeNode n in addedNodes) {
+                if (!n.NodeFont.Bold) {
+                    n.BackColor = SystemColors.Highlight;
+                    n.ForeColor = SystemColors.HighlightText;
+                    selectedNodes.Add(n);
+                }
+            }
+            foreach (TreeNode n in removedNodes) {
+                n.BackColor = treeViewMultiSelect.BackColor;
+                n.ForeColor = treeViewMultiSelect.ForeColor;
+                selectedNodes.Remove(n);
+            }
+        }
+    }
+}
